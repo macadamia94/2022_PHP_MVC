6cd5909a6778460a12c037d4857186221823d91d
macadamia94 <thswlgus15@hanmail.net>
2022-06-27 18:10:38 +0900

    .

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,1 +1,8 @@
-<h1>header</h1>
+<div>
+  <?php if(isset($_SESSION[_LOGINUSER])) {?>
+    <a href="/board/write">Write</a>
+    <a href="/user/logout">Logout</a>
+  <?php } else {?>
+
+  <?php } ?>
+</div>
